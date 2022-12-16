8bbd51895c42894f867457cd1799b3a729a98baa
mhmmdbircan <100215553+mhmmdbircan@users.noreply.github.com>
2022-12-16 14:41:33 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,29 @@
 
+public static void main(String[] args) {
+ 
+//dosya yolu
+String dosyaYolu = "C://YazilimBilisim//test2.txt";
+ 
+/*DOSYA OLUŞTURMA VE YAZMA*/
+try
+{
+FileOutputStream fos = new FileOutputStream(dosyaYolu);
+ 
+OutputStreamWriter osw=new OutputStreamWriter(fos,"UTF-8");
+ 
+osw.write("yazılım bilişim programlama 2");
+osw.close();
+}
+catch (UnsupportedEncodingException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+catch(FileNotFoundException ex)
+{
+System.out.println("Hata : " + ex);
+} 
+catch (IOException e) {
+// TODO Auto-generated catch block
+e.printStackTrace();
+}
+}
